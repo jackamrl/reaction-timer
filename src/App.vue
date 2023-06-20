e771c7f3ab9a8543e3357dd5626f7975bbc8ae36
Londou Jacques BOYODI <88946894+jackamrl@users.noreply.github.com>
2023-06-20 13:09:12 +0000

    setup timer block

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <script >
+import Block from "./components/Block.vue";
 export default {
   name: "App",
-  components: {},
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: 0,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    }
+  }
 };
+
 </script>
 
 <template>
-  <h1>JackDev reaction Timer</h1>
+  <div class="content">
+    <h1>JackDev reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" :delay="delay" />
+  </div>
 </template>
 
-<style >
-h1 {
-  text-align: center;
+<style  scoped lang="scss">
+.content {
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    text-align: center;
+  }
+
+  button {
+    padding: 0 40px;
+    height: 45px;
+    margin: 0 auto;
+    font-size: 1rem;
+    border-radius: 10px;
+    background-color: transparent;
+    color: white;
+    border: 2px solid#fff;
+    outline: none;
+    cursor: pointer;
+  }
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-if="showBlock" class="block">
+        click me
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true;
+        }, this.delay);
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.block {
+    width: 400px;
+    background-color: rgb(124, 116, 67);
+    border-radius: 10px;
+    margin: 40px auto;
+    text-align: center;
+    line-height: 100px;
+    font-size: 20px;
+    color: white;
+    padding: 100px 0;
+}
+</style>
