1d4806a3386f96363011acc6434378228c2e2aa5
Londou Jacques BOYODI <88946894+jackamrl@users.noreply.github.com>
2023-06-20 12:01:29 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script >
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<template>
+  <h1>JackDev reaction Timer</h1>
+</template>
+
+<style >
+h1 {
+  text-align: center;
+}
+</style>
